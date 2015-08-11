98e7f131c94726d441011d56c48911599b4b909c
ncharabaruk <nicholas.charabaruk@uoit.ca>
2015-08-11 14:54:51 -0400

    Move launch params into yaml files, and updated code from test results

--- omnimaxbot_control/src/approach.cpp
+++ omnimaxbot_control/src/approach.cpp
@@ -22,13 +22,15 @@ float zDist;
 //global variable for callback
 bool metGoal = false;
 
+bool goalReached = false;
+
 //lines the forks up with the can
 int line_up_x()
 {
   bool isLinedUp = false;
-  double goal = 0.04; //found experimentally
+  double goal = 0.0472; //found experimentally
   double error;
-  double tolerance = 0.0025;
+  double tolerance = 0.0381; // +/-1.5 inches
 
   geometry_msgs::Twist vel;
   vel.linear.y = 0.0;
@@ -43,22 +45,22 @@ int line_up_x()
   while (ros::ok() && isLinedUp == false)
   {
     error = xDist - goal;
-    ROS_INFO_STREAM("goal = " << goal);
+    //ROS_INFO_STREAM("goal = " << goal);
     //ROS_INFO_STREAM("xDist = " << xDist);
     //ROS_INFO_STREAM("Error = " << error);
 
     if ((error > 0 && error <= tolerance) || (error < 0 && error > -tolerance))
     {
       isLinedUp = true;
-      vel.linear.x = 0.0;
+      vel.linear.x = 0.001;
     }
     else if (error < 0)
     {
-      vel.linear.x = -0.10;
+      vel.linear.x = -0.05;
     }
     else
     {
-      vel.linear.x = 0.10;
+      vel.linear.x = 0.05;
     }
     vel_pub.publish(vel); 
   }
@@ -68,14 +70,17 @@ int line_up_x()
   return 0;
 } 
 
-int approach(double goal)
+int approach(double goalY)
 {
   bool isClose = false;
-  double error;
-  double tolerance = 0.025;
+  bool xTol = false;
+  double errorX;
+  double errorY;
+  double toleranceY = 0.025;
+  double toleranceX = 0.0381;
+  double goalX = 0.0472;
 
   geometry_msgs::Twist vel;
-  vel.linear.x = 0.0;
   vel.linear.z = 0.0;
   vel.angular.x = 0.0;
   vel.angular.y = 0.0;
@@ -86,22 +91,39 @@ int approach(double goal)
 
   while (ros::ok() && isClose == false)
   {
-    error = goal - yDist;
+    errorY = goalY - yDist;
+    errorX = xDist - goalX;
 
-    if ((error > 0 && error <= tolerance) || (error < 0 && error > -tolerance))
+    if ((errorX > 0 && errorX <= toleranceX) || (errorX < 0 && errorX > -toleranceX))
+    {
+      vel.linear.x = 0.0;
+      xTol = true;
+    }
+    else if (errorX < 0)
+    {
+      vel.linear.x = -0.05;
+      xTol = false;
+    }
+    else
+    {
+      vel.linear.x = 0.05;
+      xTol = false;
+    }
+      
+    if ((errorY > 0 && errorY <= toleranceY) || (errorY < 0 && errorY > -toleranceY))
     {
       isClose = true;
       vel.linear.y = 0.0;
     }
-    else if (error < 0)
+    else if (errorY < 0 && xTol == true)
     {
       vel.linear.y = -0.1;
     }
-    else
+    else if (errorY > 0 && xTol == true)
     {
       vel.linear.y = 0.1;
     }
-    vel_pub.publish(vel);
+    vel_pub.publish(vel); 
   }
 
   spinner.stop();
@@ -149,6 +171,10 @@ void fork_callback(const std_msgs::Bool::ConstPtr& msg)
   metGoal = msg->data;
 }
 
+void goal_callback(const std_msgs::Bool::ConstPtr& msg)
+{
+  goalReached = msg->data;
+}
 
 int main(int argc, char** argv)
 {
@@ -163,22 +189,37 @@ int main(int argc, char** argv)
   //setup subscriber
   ros::Subscriber goal_sub = n.subscribe("fork_goal_reached", 1, fork_callback);
 
+  //setup subscriber
+  ros::Subscriber move_goal_sub = n.subscribe("goal_reached", 1, goal_callback);
+
   //setup fork goal publisher
   fork_pub = n.advertise<std_msgs::Float32>("fork_position", 1);
 
   //setup velocity publisher
   vel_pub = n.advertise<geometry_msgs::Twist>("omni_cmd_vel", 1);
 
-  line_up_x();
+  ros::AsyncSpinner spinner(4);
+  spinner.start();
 
-  approach(0.25); //found experimentally
-/*
-  lift(0.0508); //lift 2 inches
+  while(ros::ok())
+  {
+    if(goalReached)
+    {
+      line_up_x();
 
-  lift(-0.0508); //put back down
+      approach(0.25); //found experimentally
 
-  approach(0.90); //reverse enough for the forks to clear the can
-*/
+      /*
+        lift(0.0508); //lift 2 inches
+
+        lift(-0.0508); //put back down
+
+        approach(0.90); //reverse enough for the forks to clear the can
+      */
+    }
+  }
+
+  spinner.stop();
     
   return 0;
 }
